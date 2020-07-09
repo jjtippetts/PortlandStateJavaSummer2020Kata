7f89bbe39f20ea8b01c2827b1ab28d70536fc367
Jordan Tippetts <jjtippetts@gmail.com>
2020-07-08 20:49:36 -0700

    formatted code

--- RomanNumerals/src/main/java/edu/pdx/cs410J/tippet2/Kata.java
+++ RomanNumerals/src/main/java/edu/pdx/cs410J/tippet2/Kata.java
@@ -2,30 +2,28 @@ package edu.pdx.cs410J.tippet2;
 
 /**
  * A class for getting started with a code kata
- *
+ * <p>
  * Use IntelliJ's "Refactor | Rename..." command to change the name of this
  * class (and its tests).
  */
 public class Kata {
-                                                                                    
 
-  public static void main(String[] args) {
-    System.err.println("Missing command line arguments");
-    System.exit(1);
-  }
 
-  public static String numberToRomanNumeral(int num)
-  {
-    StringBuilder romanNumeral = new StringBuilder();
-    if(num < 5){
-      for(int i = 0; i < num; ++i){
-        romanNumeral.append("I");
-      }
-      return romanNumeral.toString();
+    public static void main(String[] args) {
+        System.err.println("Missing command line arguments");
+        System.exit(1);
     }
-    else if(num == 5)
-      return "V";
-    else
-      return "X";
-  }
+
+    public static String numberToRomanNumeral(int num) {
+        StringBuilder romanNumeral = new StringBuilder();
+        if (num < 5) {
+            for (int i = 0; i < num; ++i) {
+                romanNumeral.append("I");
+            }
+            return romanNumeral.toString();
+        } else if (num == 5)
+            return "V";
+        else
+            return "X";
+    }
 }
