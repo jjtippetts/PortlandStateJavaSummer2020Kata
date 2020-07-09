abe344e0dffc1620391a569c0cf8edb987aa3b37
irisn <iris.ngarden@gmail.com>
2020-07-08 20:14:28 -0700

    Added "return I" test

--- RomanNumerals/src/main/java/edu/pdx/cs410J/tippet2/Kata.java
+++ RomanNumerals/src/main/java/edu/pdx/cs410J/tippet2/Kata.java
@@ -13,4 +13,9 @@ public class Kata {
     System.err.println("Missing command line arguments");
     System.exit(1);
   }
+
+  public static String numberToRomanNumeral(int num)
+  {
+    return "I";
+  }
 }
