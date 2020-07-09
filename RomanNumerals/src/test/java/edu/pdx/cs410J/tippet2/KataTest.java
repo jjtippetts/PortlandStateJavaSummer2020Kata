0c942a652a11e3fc4c458a6f0e0db5b5424dca67
irisn <iris.ngarden@gmail.com>
2020-07-08 20:17:01 -0700

    added first unit test

--- RomanNumerals/src/test/java/edu/pdx/cs410J/tippet2/KataTest.java
+++ RomanNumerals/src/test/java/edu/pdx/cs410J/tippet2/KataTest.java
@@ -2,12 +2,20 @@ package edu.pdx.cs410J.tippet2;
 
 import org.junit.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
 public class KataTest
 {
 
   @Test
   public void canInstantiateKataClass() {
     new Kata();
+  }
+
+  @Test
+  public void RomanNumeralfor1isI(){
+    assertThat(Kata.numberToRomanNumeral(1), equalTo("I"));
   }
 
 }
